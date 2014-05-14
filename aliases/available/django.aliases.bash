eb2e0fdb61f147c7b10f54ee36b68326ec266a69
James Baker <omgitsu@gmail.com>
2014-05-14 12:31:05 -0700

    couple small changes

--- aliases/available/django.aliases.bash
+++ aliases/available/django.aliases.bash
@@ -4,7 +4,7 @@ about-alias 'django specific shortcuts'
 #django shortcuts
 alias runserver='python manage.py runserver'
 alias syncdb='python manage.py syncdb'
-alias killserver='pkill -9 runserver'   ## requires pkill
+alias killserver='pkill -9 -f runserver'   ## requires pkill
 alias south='python manage.py schemamigration'
 alias startapp='python manage.py startapp'
 alias migrate='python manage.py migrate'
